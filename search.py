45834f6d712e004b08519da256cc292fea39ba89
m090009 <m090009@gmail.com>
2019-02-08 13:19:23 +0400

    Implement the path to goal iterative logic

--- search.py
+++ search.py
@@ -18,17 +18,17 @@
 import random
 import numpy as np
 
-# grid = [[0, 0, 1, 0, 0, 0],
-#         [0, 0, 1, 0, 0, 0],
-#         [0, 0, 0, 0, 1, 0],
-#         [0, 0, 1, 1, 1, 0],
-#         [0, 0, 0, 0, 1, 0]]
-
 grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
         [0, 0, 1, 0, 1, 0],
-        [0, 0, 1, 0, 1, 0],
-        [0, 0, 1, 0, 1, 0]]
+        [0, 0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1, 0]]
+
+# grid = [[0, 0, 1, 0, 0, 0],
+#         [0, 0, 0, 0, 0, 0],
+#         [0, 0, 1, 0, 1, 0],
+#         [0, 0, 1, 0, 1, 0],
+#         [0, 0, 1, 0, 1, 0]]
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
 cost = 1
@@ -48,14 +48,16 @@ def search(grid,init,goal,cost):
 
     # Init path to goal 
     path_to_goal = [[' ' if grid[row][col] == 0 else ',' for col in range(len(grid[0]))] for row in range(len(grid))]
-    print(grid)
-    print(path_to_goal)
+    # print(grid)
+    # print(path_to_goal)
     
     # Initialize a grid of the same size as the main grid to contain the parent of each cell
     parent_cache = [[grid[row][col] if grid[row][col] == 0 else ',' for col in range(len(grid[0]))] for row in range(len(grid))]
-
+    # Initialize the starting cell to 0 to indicate that it has no parent
+    parent_cache[init[0]][init[1]] = 0
+    # print(parent_cache)
     # Initialize an open grid 
-    closed_states = grid.copy()
+    closed_states = list(grid)
     # set the first initial state to closed
     closed_states[init[0]][init[1]] = 1
     # Initializing the current state with the initial state
@@ -98,14 +100,53 @@ def search(grid,init,goal,cost):
             # Expand more
             else: 
                 # Expand cell to open neighbours
-                expanded_neighbours = find_next_neighbours(current_cell[1], current_cell[2], current_cell[0], closed_states, cost, parent_cache)
+                expanded_neighbours = find_next_neighbours(current_cell[1],
+                                                           current_cell[2],
+                                                           current_cell[0],
+                                                           closed_states,
+                                                           cost,
+                                                           parent_cache)
                 # Append the expanded neighbours to the open cells list
                 open_cells.extend(expanded_neighbours)
 
     # x = [[3,1,1], [0, 1,1], [1, 1,1], [1, 1,1], [2, 1,1]]
     # x.sort(reverse=True)
     # print(expand)
-    print(parent_cache)
+
+    # Build a directional path from start to goal 
+    has_parent = True
+    # Init child cell with the goal state 
+    child_cell = (goal[0], goal[1])
+    # Add the goal symbol to the path_to_goal grid
+    path_to_goal[child_cell[0]][child_cell[1]] = '*'
+    # Backtrack cells' parents until you reach the starting cell
+    # print(parent_cache)
+    while has_parent:
+        # Check if the child_cell (current cell) has a parent
+        # print(parent_cache[child_cell[0]][child_cell[1]])
+        # print(child_cell)
+        if parent_cache[child_cell[0]][child_cell[1]] != 1 and parent_cache[child_cell[0]][child_cell[1]] != 0:
+            # 1. get the parent 
+            parent_r = parent_cache[child_cell[0]][child_cell[1]][0]
+            parent_c = parent_cache[child_cell[0]][child_cell[1]][1]
+
+            # 2. Add direction from the parent cell to the child_cell
+            # we will do this by subtracting the parent position from the child position 
+            dr = child_cell[0] - parent_r
+            dc = child_cell[1] - parent_c
+            # Get the index of the pair from the list delta
+            position_index = delta.index([dr, dc])
+            # Get the corresponding symbol from the delta name list and set out parent direction in 
+            # path_to_goal
+            path_to_goal[parent_r][parent_c] = delta_name[position_index]
+
+            # 3. Update the child cell to the parent cell
+            child_cell = (parent_r, parent_c)
+        else : 
+            # End the loop because we've reached the starting point
+            has_parent = False
+    for i in range(len(path_to_goal)):
+            print(path_to_goal[i][:])
     return current_cell
 
 
@@ -124,6 +165,7 @@ def find_next_neighbours(r, c, g, closed_states, cost, parent_cache):
                 closed_states[n_r][n_c] = 1
                 # Add the cell parent to parent_cache grid
                 parent_cache[n_r][n_c] = [r, c]
+
     # return all the possible neighbours of the given cell
     return neighbours
 
